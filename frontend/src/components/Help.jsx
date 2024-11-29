8e1dea614d0b0cd0b84aae72853dda53efaac96c
abbey-cowles <acowles1@umbc.edu>
2024-11-29 13:15:29 -0500

    Help Center is complete.

--- frontend/src/components/Help.jsx
+++ frontend/src/components/Help.jsx
@@ -10,17 +10,30 @@ const Help = () => {
           <h1 className="title">Help Center</h1>
         </div>
         <div className="faq">
-          <h3>1. FAQ 1?</h3>
-          <p>Answer to FAQ 1</p>
+          <h3>1. What is Shirt Simplify?</h3>
+          <p>Shirt Simplify is an easy to use, quick solution for reducing custom t-shirt costs.
+            It works by reducing your designs into fewer colors while maintaining original 
+            structure and subject of image. The reduction of colors will cost less in production
+            as most sites charge you per color printed.</p>
 
-          <h3>2. FAQ 2?</h3>
-          <p>Answer to FAQ 2</p>
+          <h3>2. How do I use Shirt Simplify?</h3>
+          <p>It's very simple! Go to the 'Simplify' page which can be accessed in the navigation
+             dropdown on the upper right of the screen. Upload the file of your design and select
+             a number of colors to reduce your design into. Next, you wait for us to work out magic!
+             A downloadable file will be given to you for upload into your custom shirt website of
+             choice. That's it!
+          </p>
 
-          <h3>3. FAQ 3?</h3>
-          <p>Answer to FAQ 3</p>
+          <h3>3. What type of image can I upload?</h3>
+          <p> Any common file type such as: JPG, JPEG, PNG, HEIC, HEIF. The outputted file will be a 
+            JPEG.
+          </p>
 
-          <h3>4. FAQ 4?</h3>
-          <p>Answer to FAQ 4</p>
+          <h3>4. How do I save designs for later?</h3>
+          <p>You must have an account with us if you'd like to save your designs. It's easy! Go to the
+            'Sign Up' page, enter your information, and you're all set. As soon as you upload and output
+            a design, you will be able to save it to your account.
+          </p>
         </div>
         <div className="feedback-link">
           <a href="/contact" className="link">
