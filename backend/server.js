7712a46569b4f07009abd1456a17ae7ea65c77da
abbey-cowles <158493040+abbey-cowles@users.noreply.github.com>
2024-12-01 20:54:53 -0500

    Add all posts to server.js

--- backend/server.js
+++ backend/server.js
@@ -91,6 +91,71 @@ app.post('/upload', upload.single('image'), async (req, res) => {
     }
 });
 
+app.post('/signup', (req,res) => {
+    const email = req.body.email
+    const password = req.body.password
+    db.query("SELECT * FROM users WHERE email = ?", [email], (err, results) => {
+        if (err) {
+          console.error("Database error:", err);
+          return res.status(500).send({ message: "Database error" });
+        }
+    
+        if (results.length > 0) {
+          return res.status(400).send({ message: "Email is already in use" });
+        }
+
+        db.query(
+          "INSERT INTO users (email, password) VALUES (?, ?)",
+          [email, password],
+          (err, result) => {
+            if (err) {
+              console.error("Error inserting user:", err);
+              return res.status(500).send({ message: "Error signing up" });
+            }
+    
+            res.status(200).json({ success: true, message: "User Signed up"});
+          }
+        );
+    });
+ });
+
+
+app.post('/signin', (req,res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    db.query("SELECT * FROM users WHERE email = ?", [email], (err, results) => {
+        if (err) {
+          return res.status(500).send({message: "Database error" });
+        }
+    
+        if (results.length === 0) {
+          return res.status(400).send({message: "Invalid email or password" });
+        }
+    
+        const user = results[0];
+    
+        if (user.password === password) {
+            res.status(200).json({success: true});
+        } 
+        else {
+          return res.status(400).send({message: "Invalid email or password" });
+        }
+    });
+});
+
+app.post("/contact", (req, res) => {
+    const { name, email, message } = req.body;
+
+    db.query("INSERT INTO contact (name, email, message) VALUES (?, ?, ?)", [name, email, message], (err, result) => {
+      if (err) {
+        console.error("Database error:", err);
+        return res.status(500).send({message:"Failed to send message"});
+      }
+      res.status(200).json({success: true, message: "Message Sent"});
+    });
+});
+
 app.listen(8081, () => {
     console.log("Server running on port 8081");
 });
